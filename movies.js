10cb690de9969268631b961a152489c57bfa1a5f
Kasey Agapinan <agapinank@gmail.com>
2020-05-11 13:36:26 -0700

    comments added
    Signed-off-by: Kasey Agapinan <agapinank@gmail.com>

--- movies.js
+++ movies.js
@@ -1,12 +1,10 @@
 let movies =['Jumanji','Twilight','Notebook']
 
-module.exports = movies 
+module.exports = movies //allows movies array available to other files
 
-let food = require('./food.js');
+let food = require('./food.js'); // syntax to require acceptance of another JS file 
 let songs = require('./songs.js');
 let games = require('./games.js');
 
-console.log(food);
-console.log(songs);
-console.log(games);
-console.log(movies);
+console.log(food,songs,games,movies); // prints arrays to console
+
